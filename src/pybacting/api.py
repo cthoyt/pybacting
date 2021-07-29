1b700b1ec6bcb6d4e583335094bdea900fb694f5
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-29 14:51:23 +0200

    Fix typo
    Co-Authored-By: Egon Willighagen <26721+egonw@users.noreply.github.com>

--- src/pybacting/api.py
+++ src/pybacting/api.py
@@ -1,6 +1,6 @@
 from scyjava import config, jimport
 
-config.add_endpoints('io.github.egonw.bacting:managers-cfk:0.0.20')
+config.add_endpoints('io.github.egonw.bacting:managers-cdk:0.0.20')
 workspace_root = '.'
 CDK = jimport('net.bioclipse.managers.CDKManager')
 cdk = CDK(workspace_root)
