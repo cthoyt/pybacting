0f18b0ba72ecb618a0221b328735ae13812ef26c
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-07-31 10:12:02 +0200

    Update __init__.py

--- src/pybacting/__init__.py
+++ src/pybacting/__init__.py
@@ -2,5 +2,5 @@
 
 """A python wrapper around `bacting <https://github.com/egonw/bacting>`_."""
 
-from .api import cdk  # noqa:F401
+from .api import cdk, inchi, opsin, pubchem  # noqa:F401
 from .io import from_iupac_name, from_smiles, get_inchi, get_inchi_key  # noqa:F401
