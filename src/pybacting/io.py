d6d925a5280d98b11bbf7c79d15c63198ef81550
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-01 14:48:16 +0200

    Unbox java string

--- src/pybacting/io.py
+++ src/pybacting/io.py
@@ -4,6 +4,7 @@
 
 from typing import Any
 
+from scyjava import to_python
 from .api import cdk, inchi, opsin
 
 Molecule = Any
@@ -30,8 +31,8 @@ def get_inchi_key(mol: Molecule) -> str:
 
 
 def get_svg(mol: Molecule) -> str:
-    """Get an SVG depictiong for a molecule."""
-    return cdk.asSVG(mol)
+    """Get an SVG depicting for a molecule."""
+    return to_python(cdk.asSVG(mol))
 
 
 if __name__ == "__main__":
