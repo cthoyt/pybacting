89fadc80a8422edd9edf1cc5bba7611ba91b5ba3
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-03 22:17:59 +0100

    Update __init__.py

--- src/pybacting/__init__.py
+++ src/pybacting/__init__.py
@@ -2,5 +2,17 @@
 
 """A python wrapper around `bacting <https://github.com/egonw/bacting>`_."""
 
-from .api import biojava, bridgedb, cdk, excel, inchi, opsin, oscar, pubchem, qudt, rdf, xml  # noqa:F401
+from .api import (  # noqa:F401
+    biojava,
+    bridgedb,
+    cdk,
+    excel,
+    inchi,
+    opsin,
+    oscar,
+    pubchem,
+    qudt,
+    rdf,
+    xml,
+)
 from .io import from_iupac_name, from_smiles, get_inchi, get_inchi_key, get_svg  # noqa:F401
