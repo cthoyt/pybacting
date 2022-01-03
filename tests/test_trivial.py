4646ddef07b5cba055f370ec251ed48afbdaf034
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-03 22:24:33 +0100

    Update test_trivial.py

--- tests/test_trivial.py
+++ tests/test_trivial.py
@@ -5,6 +5,7 @@
 import unittest
 
 import pybacting
+from pybacting import qudt
 
 
 class TestImport(unittest.TestCase):
@@ -26,3 +27,10 @@ class TestImport(unittest.TestCase):
     #     mol = pybacting.from_iupac_name("methane")
     #     self.assertEqual("InChI=1S/CH4/h1H4", pybacting.get_inchi(mol))
     #     self.assertEqual("VNWKTOKETHGBQD-UHFFFAOYSA-N", pybacting.get_inchi_key(mol))
+
+    def test_qudt_manager_name(self):
+        """Test getting the qudt manager name.
+
+        .. seealso:: https://egonw.github.io/bacting-api/net/bioclipse/managers/QUDTManager.html
+        """
+        self.assertIsInstance(qudt.getManagerName(), str)
