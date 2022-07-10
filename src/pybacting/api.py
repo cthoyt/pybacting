bd34db19a174d356e6fd89a872ff560786d58a35
Egon Willighagen <egon.willighagen@gmail.com>
2022-07-10 16:49:06 +0200

    Bacting 0.0.41: Hardcoded org.eclipse.equinox.preferences 3.10.1

--- src/pybacting/api.py
+++ src/pybacting/api.py
@@ -22,7 +22,7 @@ __all__ = [
 ]
 
 WORKSPACE = "."
-VERSION = "0.0.40"
+VERSION = "0.0.41"
 
 # The ones marked with "no" can't be loaded because they are POM-only
 # artifacts. See the excellent explanation given by @ctrueden why at:
