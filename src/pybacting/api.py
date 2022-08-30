4dbd753eddea57fb2c995ace51bc15309ed56ef3
Egon Willighagen <egon.willighagen@gmail.com>
2022-08-30 11:08:14 +0200

    0.0.44 is released now

--- src/pybacting/api.py
+++ src/pybacting/api.py
@@ -19,7 +19,7 @@ __all__ = [
 ]
 
 WORKSPACE = "."
-VERSION = "0.0.42"
+VERSION = "0.0.44"
 
 # The ones marked with "no" can't be loaded because they are POM-only
 # artifacts. See the excellent explanation given by @ctrueden why at:
